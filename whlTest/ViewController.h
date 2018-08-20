9160225fb0222702a05c128b629844d23fe2bf5b
王洪亮 <867561903@qq.com>
2018-08-20 19:47:22 +0800

    Initial Commit

---
+++ whlTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  whlTest
+//
+//  Created by 王洪亮 on 2018/8/20.
+//  Copyright © 2018年 wanghongliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ whlTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  whlTest
+//
+//  Created by 王洪亮 on 2018/8/20.
+//  Copyright © 2018年 wanghongliang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
